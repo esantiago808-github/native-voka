aec083a04d2d42436a4e140a01c52834345228a6
Edith Santiago <esantiago808@gmail.com>
2020-08-19 21:59:29 -1000

    HOA

--- components/HOADuesComponent.js
+++ components/HOADuesComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { ScrollView, View, Linking } from 'react-native';
+import { Text, Card, Icon } from 'react-native-elements';
 
 
 class HoaDues extends Component {
@@ -11,9 +11,84 @@ class HoaDues extends Component {
 
     render() {
         return (
-            <View>
-                <Text h3>Home Owners Association</Text>
-            </View>
+            <ScrollView>
+                <Card 
+                    title='Home Owners Association'
+                    titleStyle={{ fontWeight: "bold", fontSize: 24 }}
+                    image={require("../shared/images/villagesofkapolei-aerial.jpg")}
+                >
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        The pride of ownership of the Villages of Kapolei are paid for by our homeowners. These dues ensure that trees, roads, sidewalks, parks, recreational facilities, to name a few, are well maintained to be enjoyed by our families and guests.
+                    </Text>
+                    <View style={{ flex: 1, flexDirection: "row", alignItems: 'center', justifyContent: 'center' }}>
+                        <Icon 
+                            name='credit-card'
+                            type='font-awesome'
+                            color='#017f1b'
+                            raised
+                            reverse
+                            onPress={() => Linking.openURL('https://owner.topssoft.com/VillagesofKapolei/Account/Login')}
+                        />
+                    </View>
+                    <Text style={{ flex: 1, flexDirection: "row", textAlign: 'center' }}
+                            onPress={() => Linking.openURL('https://owner.topssoft.com/VillagesofKapolei/Account/Login')}>
+                        HOA Payment
+                    </Text>
+                </Card>
+
+                <Card 
+                    title='Covenants and Design Rules'
+                    titleStyle={{ fontWeight: "bold", fontSize: 24 }}
+                    image={require("../shared/images/villagesofkapolei-aerial.jpg")}
+                >
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Please visit our website for Covenants and Design Rules
+                    </Text> 
+                    <View style={{ flex: 1, flexDirection: "row", alignItems: 'center', justifyContent: 'center' }}>
+                        <Icon 
+                            name='list-alt'
+                            type='font-awesome'
+                            color='#e8873a'
+                            raised
+                            reverse
+                            onPress={() => Linking.openURL('https://edith-nucamp-projects.s3-us-west-1.amazonaws.com/VOKA/residents.html')}
+                        />
+                    </View>
+                    <Text style={{ flex: 1, flexDirection: "row", textAlign: 'center' }}
+                            onPress={() => Linking.openURL('https://edith-nucamp-projects.s3-us-west-1.amazonaws.com/VOKA/residents.html')}>
+                        Covenants and Design Rules
+                    </Text>
+                   
+                </Card>
+
+                <Card 
+                    title='Board of Directors'
+                    titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+                    image={require("../shared/images/villagesofkapolei-aerial.jpg")}
+                >
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Vanessa Lum, 2020 President
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Nathan Napihaa, 2022 Vice President
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        David Luegen, 2022 Secretary
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Carol Lagapa, 2021 Treasurer
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Rudy Amasol, 2021 Director
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Scarlet Godinez-Dumadag, 2020 Director
+                    </Text>
+                    <Text style={{ fontSize: 18, margin: 20 }}>
+                        Leo Domingo, 2020 Director
+                    </Text>
+                </Card>
+            </ScrollView>
         );
     }
 }
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,237 +1,177 @@
-import React, { Component } from 'react';
-import { ScrollView, Text, StyleSheet, View } from 'react-native';
-import { Card, Icon, Button } from 'react-native-elements';
-import { createStackNavigator } from 'react-navigation';
-import Events from './EventsHomeComponent';
-import HoaDues from './HOADuesComponent';
-import News from './NewsLetterComponent';
-import Contact from './ContactComponent';
-import Bulletin from './BulletinComponent';
-import Banquet from './BanquetComponent';
-import ProgramsList from './ProgramListComponent';
-import ProgramSch from './ProgramScheduleComponent';
+import React, { Component } from "react";
+import { ScrollView, Text, StyleSheet, View } from "react-native";
+import { Card, Icon, Button, Image } from "react-native-elements";
+import { createStackNavigator } from "react-navigation";
+import Events from "./EventsHomeComponent";
+import HoaDues from "./HOADuesComponent";
+import News from "./NewsLetterComponent";
+import Contact from "./ContactComponent";
+import Bulletin from "./BulletinComponent";
+import Banquet from "./BanquetComponent";
+import ProgramsList from "./ProgramListComponent";
+import ProgramSch from "./ProgramScheduleComponent";
 
-function About({props}) {
-
-        return (
-            <ScrollView>
-                <Card
-                    title='Villages of Kapolei'
-                    wrapperStyle={{margin: 20}}
-                    image={require('../shared/images/villagesofkapolei-aerial.jpg')}
-                    >
-                    <Text style={{ fontSize: 18 }}>
-                        The Villages of Kapolei is a Planned Community and part of the new city located on the Ewa plains of Oahu, Hawaii. The community features single family homes, condominiums, and rental apartments, approximately 5,000 units. Our community continues to grow as Kapolei, Second City is under development. The master plan community consists of the highest rated elementary, middle and high schools in the state of Hawaii, along with churches, parks, recreation centers, nearby retail centers, restaurants, and a 4 1/2-star Golf Digest rated golf course spread over 888-acres of land.
-                    </Text>
-                </Card>
-                <Card 
-                    wrapperStyle={{margin: 20}}
-                    image={require('../shared/images/kapolei-golf.jpg')}
-                    >
-                    <Text style={{ fontSize: 18 }}>
-                        The Villages of Kapolei is comprised of 8 Villages:
-
-                        Kumu Iki,
-                        Aeloa,
-                        Malanai,
-                        Kekuilani,
-                        Iwalani,
-                        Malu’ohai,
-                        Kapolei Kai,
-                        Kaupe’a
-                    </Text>
-                </Card>
-                <Card 
-                    wrapperStyle={{margin: 20}}
-                    image={require('../shared/images/kapolei-commons.jpg')}
-                    >
-                    <Text style={{ fontSize: 19 }}>
-                        With many of the businesses adding a second location or moving to Kapolei, our residents are enjoying the decreased commute and improved family time.
-                    </Text>
-                </Card>
-            </ScrollView>
-        );
-    }
-
-
-class Home extends Component {
-
-    static navigationOptions = {
-        title: 'Home'
-    }
-
-    render() {
-
-        const { navigate } = this.props.navigation;
-
-        return (
-            <ScrollView>
-                <Card>
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Icon
-                            raised
-                            name='info-circle'
-                            type='font-awesome'
-                            color='#72B4AC'
-                            reverse
-                            onPress={() => navigate('HoaDues')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='calendar'
-                            type='font-awesome'
-                            color='#3a6e93'
-                            reverse
-                            onPress={() => navigate('Events')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='calendar'
-                            type='font-awesome'
-                            color='#e8873a'
-                            reverse
-                            onPress={() => navigate('ProgramSch')} 
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-                    </View>
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Text 
-                            style={styles.labelText} 
-                            onPress={() => navigate('HoaDues')}> 
-                              HOA
-                        </Text>
-                        <Text 
-                            style={{marginHorizontal: 20}} 
-                            onPress={() => navigate('Events')}> 
-                            Events 
-                        </Text>
-                        <Text onPress={() => navigate('ProgramSch')}>  Program Schedule </Text>
-                    </View>
-
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Icon
-                            raised
-                            name='address-card'
-                            type='font-awesome'
-                            color='#f6d65b'
-                            reverse
-                            onPress={() => navigate('Contact')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='list-alt'
-                            type='font-awesome'
-                            color='#017f1b'
-                            reverse
-                            onPress={() => navigate('ProgramsList')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='newspaper-o'
-                            type='font-awesome'
-                            color='#d44b19'
-                            reverse
-                            onPress={() => navigate('News')} 
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-                    </View>
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Text 
-                            style={styles.labelText} 
-                            onPress={() => navigate('Contact')}> 
-                              Contact
-                        </Text>
-                        <Text 
-                            style={{marginHorizontal: 20}} 
-                            onPress={() => navigate('ProgramsList')}> 
-                            Program List 
-                        </Text>
-                        <Text onPress={() => navigate('News')}>  Newsletter </Text>
-                    </View>
-                    
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Icon
-                            raised
-                            name='newspaper-o'
-                            type='font-awesome'
-                            color='#e8873a'
-                            reverse
-                            onPress={() => navigate('Bulletin')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='heartbeat'
-                            type='font-awesome'
-                            color='#72B4AC'
-                            reverse
-                            onPress={() => navigate('Contact')}
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-
-                        <Icon
-                            raised
-                            name='heartbeat'
-                            type='font-awesome'
-                            color='#3a6e93'
-                            reverse
-                            onPress={() => navigate('Contact')} 
-                            containerStyle={{
-                                margin: 25 
-                            }} 
-                        />
-                    </View>
-                    <View style={{flex: 1, flexDirection: 'row'}}>
-                        <Text 
-                            style={styles.labelText} 
-                            onPress={() => navigate('Bulletin')}> 
-                              Bulletin Board
-                        </Text>
-                        <Text 
-                            style={{marginHorizontal: 20}} 
-                            onPress={() => navigate('Contact')}> 
-                            Contact
-                        </Text>
-                        <Text onPress={() => navigate('Contact')}>  Activity </Text>
-                    </View>
-
-                </Card>
-                <About />
-            </ScrollView>
-        );
-    }
+function About({ props }) {
+  return (
+    <ScrollView>
+      <Card
+        image={require("../shared/images/villagesofkapolei-aerial.jpg")}
+      >
+        <Text style={{ fontSize: 18, margin: 20}}>
+          The Villages of Kapolei is a Planned Community and part of the new
+          city located on the Ewa plains of Oahu, Hawaii. The community features
+          single family homes, condominiums, and rental apartments,
+          approximately 5,000 units. Our community continues to grow as Kapolei,
+          Second City is under development. The master plan community consists
+          of the highest rated elementary, middle and high schools in the state
+          of Hawaii, along with churches, parks, recreation centers, nearby
+          retail centers, restaurants, and a 4 1/2-star Golf Digest rated golf
+          course spread over 888-acres of land.
+        </Text>
+      </Card>
+      <Card
+        image={require("../shared/images/kapolei-golf.jpg")}
+      >
+        <Text style={{ fontSize: 18, margin: 20 }}>
+          The Villages of Kapolei is comprised of 8 Villages: Kumu Iki, Aeloa,
+          Malanai, Kekuilani, Iwalani, Malu’ohai, Kapolei Kai, Kaupe’a
+        </Text>
+      </Card>
+      <Card
+        image={require("../shared/images/kapolei-commons.jpg")}
+      >
+        <Text style={{ fontSize: 18, margin: 20 }}>
+          With many of the businesses adding a second location or moving to
+          Kapolei, our residents are enjoying the decreased commute and improved
+          family time.
+        </Text>
+      </Card>
+    </ScrollView>
+  );
 }
 
-const styles = StyleSheet.create({
-    labelText: {
-        textAlign: 'center', 
-        marginHorizontal: 25,
-    }
-  });
+class Home extends Component {
+  static navigationOptions = {
+    title: "Home",
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <ScrollView>
+        <Card
+          title="Villages of Kapolei Association"
+          titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+          image={require("../shared/images/voka-icon.jpg")}
+        >
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Icon
+              raised
+              name="info-circle"
+              type="font-awesome"
+              color="#72B4AC"
+              reverse
+              onPress={() => navigate("HoaDues")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+
+            <Icon
+              raised
+              name="calendar"
+              type="font-awesome"
+              color="#3a6e93"
+              reverse
+              onPress={() => navigate("Events")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+
+            <Icon
+              raised
+              name="cutlery"
+              type="font-awesome"
+              color="#e8873a"
+              reverse
+              onPress={() => navigate("ProgramSch")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+
+          </View>
+
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Text style={{marginHorizontal: 45}} onPress={() => navigate("HoaDues")}>
+              HOA
+            </Text>
+            <Text style={{marginHorizontal: 0}} onPress={() => navigate("Events")}>
+              Program Schedule
+            </Text>
+            <Text style={{marginHorizontal: 10}} onPress={() => navigate("ProgramSch")}>
+              Banquet Rental
+            </Text>
+          </View>
+
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Icon
+              raised
+              name="newspaper-o"
+              type="font-awesome"
+              color="#017f1b"
+              reverse
+              onPress={() => navigate("News")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+
+            <Icon
+              raised
+              name="list-alt"
+              type="font-awesome"
+              color="#d44b19"
+              reverse
+              onPress={() => navigate("News")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+            <Icon
+              raised
+              name="address-card"
+              type="font-awesome"
+              color="#f6d65b"
+              reverse
+              onPress={() => navigate("Contact")}
+              containerStyle={{
+                marginHorizontal: 25
+              }}
+            />
+
+          </View>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Text style={{marginHorizontal: 40}} onPress={() => navigate("News")}>
+              News
+            </Text>
+            <Text style={{ marginHorizontal: 15 }} onPress={() => navigate("News")}>
+              Bulletin Board
+            </Text>
+            <Text style={{ marginHorizontal: 30 }} onPress={() => navigate("Contact")}> 
+                Contact 
+            </Text>
+          </View>
+
+        </Card>
+
+        <About />
+      </ScrollView>
+    );
+  }
+}
+
 
 export default Home;
